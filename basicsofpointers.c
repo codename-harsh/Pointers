7502d278cd55d5aed23cf46b153428f1b1e7e9e3
Harshvardhan Singh <hvsingh46@gmail.com>
2025-11-23 22:08:53 +0530

    Initial commit

---
+++ basicsofpointers.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a = 789; // simply variable initialization
+    int* x = &a; //isse x me jo "a" ka address hai wo store ho jayega 
+    printf("%d",x); // %d use kia yaha pe for x yani ki bas x me jo a ka address hai wo print hoga 
+    printf("\n\n");
+    //now 
+    printf("%p",&x); // %p krne se jo x variable ka address hai wwo print hoga 
+    // we can also edit the value of the variable using this pointer / address shit :0
+    printf("\n");
+    *x = 330;
+    printf("%d",a); // note kro kii a ko hi print krana hai x ko nhi :)
+}
