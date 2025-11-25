bd1488576256fce55f50bb9815d58b044b24c1b2
Harshvardhan Singh <hvsingh46@gmail.com>
2025-11-25 20:32:44 +0530

    Update basicsofpointers.c

--- basicsofpointers.c
+++ basicsofpointers.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main(){
-    int a = 789; // simply variable initialization
-    int* x = &a; //isse x me jo "a" ka address hai wo store ho jayega 
-    printf("%d",x); // %d use kia yaha pe for x yani ki bas x me jo a ka address hai wo print hoga 
-    printf("\n\n");
-    //now 
-    printf("%p",&x); // %p krne se jo x variable ka address hai wwo print hoga 
+    int a = 789; // simply variable initialization .
+    int* x = &a; //isse x me jo "a" ka address hai wo store ho jayega .
+    printf("%d",x); // %d use kia yaha pe for x yani ki bas x me jo a ka address hai wo print hoga .
+    printf("\n\n"); // for two lines space .
+    //now isme :/
+    printf("%p",&x); // %p krne se jo x variable ka address hai wo print hoga 
     // we can also edit the value of the variable using this pointer / address shit :0
     printf("\n");
     *x = 330;
